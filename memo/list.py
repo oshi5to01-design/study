a3fac2076e5f8bd99fc9054402c3575c08c821ab
oshi5to01-design <246975035+oshi5to01-design@users.noreply.github.com>
2026-02-03 23:08:08 +0900

    メモ

---
+++ memo/list.py
@@ -0,0 +1,3 @@
+columns = [c["name"] for c in inspector.get_columns("sessions")]
+columns = [c["name"] for c in inspector.get_columns("sessions")]
+columns = [c["name"] for c in inspector.get_columns("sessions")]
