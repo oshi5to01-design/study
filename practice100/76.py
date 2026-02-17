0a0074d3ffededadc60f3042a6185030acde3d24
oshi5to01-design <246975035+oshi5to01-design@users.noreply.github.com>
2026-02-17 20:43:14 +0900

    76問目

--- practice100/76.py
+++ practice100/76.py
@@ -9,10 +9,14 @@ class Student:
     def add_score(self, subject, score):
         self.scores[subject] = score
 
+    def total_score(self):
+        return sum(self.scores.values())
+
 
 student = Student("鈴木", 2, "B")
 student.add_score("数学", 80)
 student.add_score("英語", 70)
 student.add_score("国語", 90)
 
-print(student.scores)
+
+print(student.total_score())
