8e7f39a04fc54aad034cc5f92d40ab2a79c9ae99
oshi5to01-design <246975035+oshi5to01-design@users.noreply.github.com>
2026-02-21 09:41:33 +0900

    80問目

---
+++ practice100/80.py
@@ -0,0 +1,31 @@
+import random
+
+
+class Card:
+    def __init__(self, suit, number):
+        self.suit = suit
+        self.number = number
+
+    marks = ["heart", "diya", "spade", "club"]
+    numbers = list(range(1, 14))
+
+
+class Deck:
+    def __init__(self):
+        self.cards = []
+        for suit in ["ハート", "ダイヤ", "スペード", "クラブ"]:
+            for number in range(1, 14):
+                card = Card(suit, number)
+                self.cards.append(card)
+
+    def shuffle(self):
+        random.shuffle(self.cards)
+
+    def draw_card(self):
+        return self.cards.pop(0)
+
+
+deck = Deck()
+deck.shuffle()
+for card in deck.cards:
+    print(f"{card.suit}{card.number}")
